31794f32ebedaec53a276ad352dc5ae18c7aac2b
Gabriel <barbosadevgab@gmail.com>
2023-07-10 21:19:33 -0300

    Project1

---
+++ app/src/main/java/com/example/apptest1/MainActivity.kt
@@ -0,0 +1,9 @@
+fun main() {
+   var nome = "Gabriel Barbosa"
+    var tex:String
+    val calcado:Byte = 44
+    var pib:Long = 1869000000000
+    var pop = 211000000
+    println("O PIB per capita do Brasil é : " + pib/pop)
+}
+
